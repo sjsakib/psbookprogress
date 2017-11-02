35cc61eb535d8c3017942587e9e16be2e592dd44
S.j. Sakib <sjsakib.bd@gmail.com>
2017-11-02 15:48:55 +0600

    modified Problem admin site

--- progress/admin.py
+++ progress/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from progress.models import Part, Chapter, Judge, Problem, UserProfile
 
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'chapter', 'part')
+    list_filter = ('chapter', 'part')
+    search_fields = ('name', )
+    list_per_page = 50
+
+
 admin.site.register(Part)
 admin.site.register(Chapter)
 admin.site.register(Judge)
-admin.site.register(Problem)
 admin.site.register(UserProfile)
