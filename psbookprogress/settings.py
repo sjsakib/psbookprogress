176a56fd995da9b0004a465af6a689e719175189
S.j. Sakib <sjsakib.bd@gmail.com>
2017-11-09 22:00:51 +0600

    added mysql backend

--- psbookprogress/settings.py
+++ psbookprogress/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'psbookprogress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
     }
 }
 
