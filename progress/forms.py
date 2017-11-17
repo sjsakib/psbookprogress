a8f28f7a8f28c4d6b1f75c816c5090d0687fca6c
S.j. Sakib <sjsakib.bd@gmail.com>
2017-11-17 23:48:40 +0600

    added HTML cleaner

--- progress/forms.py
+++ progress/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from progress.models import UserProfile, Tip
 
+import bleach
+
 
 class UserForm(forms.ModelForm):
 
@@ -18,8 +20,18 @@ class UserProfileForm(forms.ModelForm):
 
 
 class TipForm(forms.ModelForm):
+    allowed_tags = bleach.sanitizer.ALLOWED_TAGS + ['iframe', 'pre']
+    allowed_attributes = {**bleach.sanitizer.ALLOWED_ATTRIBUTES, **{'iframe': ['src', 'frameborder', 'allowfullscreen']}}
 
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Supports basic HTML tags'}))
+    help_text = "Supported HTML tags are: " + ', '.join(allowed_tags)
+    help_text += ".\n Use <pre> for block text, like sample input"
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': help_text}))
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        content = bleach.clean(content, tags=self.allowed_tags, attributes=self.allowed_attributes)
+
+        return bleach.linkify(content)
 
     class Meta:
         model = Tip
